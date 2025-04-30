33ba18342fada08da4283710e0d45b0fb7b88d3e
cbollengier <clementbodev@gmail.com>
2025-04-30 14:12:51 +0200

    C2 : créations des constante & du style

---
+++ public/js/HelloWorld.js
@@ -0,0 +1,5 @@
+const HELLO_WORLD_TITLE =  document.querySelector('h1');
+const HELLO_WORLD_TEXT = document.querySelector('p');
+
+HELLO_WORLD_TITLE.innerText = 'Hello World';
+HELLO_WORLD_TEXT.innerText = 'This is a simple Hello World example using JavaScript.';
